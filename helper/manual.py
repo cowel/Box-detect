5e4fc2879d957347e5b4ff42f468648169446bf0
sihcpro <tvquocchi@gmail.com>
2019-12-08 00:24:23 +0700

    add: manual

---
+++ helper/manual.py
@@ -0,0 +1,11 @@
+from .color import color
+
+
+MANUAL = '''---------- Manual ----------
+-l <data_size>
+        light mode (default data size = 10 picture)
+-h      heavy mode (full data size)
+
+-r      Rebuild model
+----------------------------
+'''
